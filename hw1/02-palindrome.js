160eb0a61b432f4e04dafabbec3764847c555a93
Rutuja Padgilwar <rpadgil2@pdx.edu>
2023-04-06 14:43:08 -0700

    update comparison operator to ===

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -3,18 +3,18 @@ inputNumber.addEventListener("input", (event) => {
   let number = document.getElementById("number").value;
   let result = document.getElementById("result");
 
-  if (Math.sign(number) == -1) {
+  if (Math.sign(number) === -1) {
     result.innerHTML = "Please enter positive number"; // validation for negative number
     result.style = "color: red";
   } else {
     let len = number.length;
     let mid = Math.floor(len / 2);
-    if (len == 0) {
+    if (len === 0) {
       result.innerHTML = "No input given";
       result.style = "color: black";
     } else {
       for (let i = 0; i <= mid; i++) {
-        if (number[i] != number[len - 1 - i]) {
+        if (number[i] !== number[len - 1 - i]) {
           result.innerHTML = "No. Try again";
           result.style = "color: red";
           break;
