16d765a59102a9c7ab23f07339398fab1474afa8
Rutuja Padgilwar <rpadgil2@pdx.edu>
2023-04-14 02:25:01 -0700

    Add javascript code to hightlight the search word and add bootstarp styling

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,11 @@
 // Add your code here
-
-input.addEventListener('keydown', handleKeyDown);
+let para = document.getElementById("paraInfo");
+let input_text = document.getElementById("userinput");
+input_text.addEventListener("input", (event) => {
+  const searchText = event.target.value;
+  const regex = new RegExp(searchText, "g");
+  let text = para.innerHTML;
+  text = text.replace(/(<mark>|<\/mark>)/gim, ""); // remove previously highlighted words
+  const newText = text.replace(regex, "<mark>$&</mark>");
+  para.innerHTML = newText;
+});
