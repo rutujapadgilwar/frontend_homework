815642d0d92d4603958ec1893f9d3c7298110136
Rutuja Padgilwar <rpadgil2@pdx.edu>
2023-04-21 12:55:26 -0700

    Update the == operator to ===

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -10,7 +10,7 @@ function changeBackgroundColor(event) {
   event.stopPropagation();
   let btnValue = document.querySelector(".btn");
   let input = document.getElementById("timeInterval");
-  if (btnValue.value == "Start") {
+  if (btnValue.value === "Start") {
     btnValue.value = "Stop";
     btnValue.className = "btn btn-danger";
     input.disabled = true;
@@ -18,7 +18,7 @@ function changeBackgroundColor(event) {
       let randomColor = Math.floor(Math.random() * 16777215).toString(10);
       document.body.style.backgroundColor = "#" + randomColor;
     }, input.value * 1000);
-  } else if (btnValue.value == "Stop") {
+  } else if (btnValue.value === "Stop") {
     btnValue.value = "Start";
     btnValue.className = "btn btn-primary";
     input.disabled = false;
