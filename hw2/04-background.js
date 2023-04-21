a69d22f9d8da628adde2c217b331fa1aab862aab
Rutuja Padgilwar <rpadgil2@pdx.edu>
2023-04-21 12:47:09 -0700

    Add javascript code for form validation and submission operation,and html code using boostrap from classes for display

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,11 +1,8 @@
-//Add your code here
 let timeInterval = setInterval(function () {
   let randomColor1 = Math.floor(Math.random() * 16777215).toString(10);
   document.body.style.backgroundColor = "#" + randomColor1;
 }, 3000);
-
 let btnSelector = document.querySelector(".btn");
-
 btnSelector.addEventListener("click", changeBackgroundColor);
 function changeBackgroundColor(event) {
   clearInterval(timeInterval);
@@ -15,7 +12,7 @@ function changeBackgroundColor(event) {
   let input = document.getElementById("timeInterval");
   if (btnValue.value == "Start") {
     btnValue.value = "Stop";
-    btnValue.className = "btn btn-danger"
+    btnValue.className = "btn btn-danger";
     input.disabled = true;
     timeInterval = setInterval(function () {
       let randomColor = Math.floor(Math.random() * 16777215).toString(10);
@@ -23,7 +20,7 @@ function changeBackgroundColor(event) {
     }, input.value * 1000);
   } else if (btnValue.value == "Stop") {
     btnValue.value = "Start";
-    btnValue.className = "btn btn-primary"
+    btnValue.className = "btn btn-primary";
     input.disabled = false;
     document.body.style.backgroundColor = "white";
   }
--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,59 @@
-// Add your code here
+function validateForm() {
+  const formatName = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~0123456789]/;
+  const formatEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let fieldName = document.forms["form1"]["Username"].value;
+  let fieldEmail = document.forms["form1"]["email"].value;
+
+  let validBool = true;
+  if (fieldName === "" || fieldName.match(formatName)) {
+    window.alert("Please enter a valid Name");
+    validBool = false;
+  }
+  if (fieldEmail === "" || !fieldEmail.match(formatEmail)) {
+    window.alert("Please enter a valid Email");
+    validBool = false;
+  }
+  return validBool;
+}
+function resetForm() {
+  document.getElementById("form1").reset();
+}
+function submitForm() {
+  if (!validateForm()) {
+    return;
+  }
+  let name = document.getElementById("Username").value;
+  let email = document.getElementById("email").value;
+  console.log("========Form Submission======");
+  console.log("Name: " + name);
+  console.log("Email: " + email);
+
+  let registrationStatus = document.getElementById("registrationStatus").value;
+  if (registrationStatus == "") {
+    console.log("Class Registration: No status");
+  } else {
+    console.log("Class Registration: ", registrationStatus);
+  }
+
+  let courseCheckbox = [];
+  courseSelection = "Course Selection: ";
+  courseCheckbox = document.getElementsByName("courseSelection");
+  const selectedCourses = [];
+  if (courseCheckbox[0].checked === true) {
+    selectedCourses.push("Programming Language");
+  }
+  if (courseCheckbox[1].checked === true) {
+    selectedCourses.push("Operating System");
+  }
+  if (courseCheckbox[2].checked === true) {
+    selectedCourses.push("Full Stack Web Developement");
+  }
+  console.log("Course Selection: " + selectedCourses.toString());
+
+  let comment = document.getElementById("comment").value;
+  if (comment.length === 0) {
+    console.log("Feedback: No feedback was submitted.");
+  } else {
+    console.log("Feedback: " + comment);
+  }
+}
