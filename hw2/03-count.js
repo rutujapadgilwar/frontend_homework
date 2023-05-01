b3cfdbf0c459fba86600c28f17e4c55aa993c813
Rutuja Padgilwar <rpadgil2@pdx.edu>
2023-04-30 21:13:00 -0700

    Update background color styling

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -5,7 +5,7 @@ inputText.addEventListener("input", (event) => {
   const searchText = event.target.value;
   const regex = new RegExp(searchText, "g");
   let text = paragraphInfo.innerHTML;
-  text = text.replace(/(<mark>|<\/mark>)/gim, "");
-  const newText = text.replace(regex, "<mark>$&</mark>");
+  text = text.replace(/(<mark style="background-color: yellow;">|<\/mark>)/gim, "");
+  const newText = text.replace(regex, `<mark style="background-color: yellow;">$&</mark>`);
   paragraphInfo.innerHTML = newText;
 });
