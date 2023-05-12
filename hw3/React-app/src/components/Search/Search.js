be842091771eda53956c3d8152b7fda41a620a5e
Rutuja Padgilwar <rpadgil2@pdx.edu>
2023-05-12 10:40:35 -0700

    Update sugessted changes

--- hw3/React-app/src/components/Search/Search.js
+++ hw3/React-app/src/components/Search/Search.js
@@ -1,94 +1,87 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 
-class Search extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            charName: [],
-            imgList: [],
-            Result: false,
-            NoResult: false,
-            value: "",
-            valueIndex: -1
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+const Search = () => {
+  const [charName, setCharName] = useState([]);
+  const [imgList, setImgList] = useState([]);
+  const [result, setResult] = useState(false);
+  const [noResult, setNoResult] = useState(false);
+  const [value, setValue] = useState("");
+  const [valueIndex, setValueIndex] = useState(-1);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const valueIndex = charName.indexOf(value);
+    if (valueIndex < 0) {
+      setResult(false);
+      setNoResult(true);
+    } else {
+      setResult(true);
+      setNoResult(false);
+      setValueIndex(valueIndex);
     }
+  };
 
-    handleChange(event) {
-        const complete = {...this.state};
-        complete.value = event.target.value;
-        this.setState(complete);
-    }
+  useEffect(() => {
+    let url = "https://thronesapi.com/api/v2/Characters";
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        const arr1 = [];
+        const arr2 = [];
+        result.forEach((element) => {
+          arr1.push(element.fullName);
+          arr2.push(element.imageUrl);
+        });
+        setCharName(arr1);
+        setImgList(arr2);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    handleSubmit(event){
-        const complete = { ...this.state };
-        console.log(complete.value);
-        complete.valueIndex = complete.charName.indexOf(complete.value);
-        console.log(complete.valueIndex);
-        if (complete.valueIndex < 0) {
-            complete.Result = false;
-            complete.NoResult = true;
-        } else {
-            complete.Result = true;
-            complete.NoResult = false;
-        }
-        this.setState(complete);
-        event.preventDefault();
-    }
-
-    componentDidMount(){
-        let url = 'https://thronesapi.com/api/v2/Characters';
-        fetch(url)
-        .then(response => response.json())
-        .then(result => {
-            let arr1 = [];
-            let arr2 = [];
-            result.forEach((element) => {
-                arr1.push(element.fullName);
-                arr2.push(element.imageUrl);
-            });
-            this.setState({ charName: arr1, imgList: arr2 });
-        })
-        .catch((error) => {
-            console.error(error);
-        })
-    }
-
-    render() {
-        return (
-            <div className="container w-75 mx-auto">
-                <h1>Search for the character</h1>
-                <div className="container-sm border rounded bg-light search-container">
-                    <form onSubmit={this.handleSubmit} className="p-3">
-                        <div className="mb-3 ">
-                            <label htmlFor="input-char">Enter the character full name:</label>
-                            <input type="text" id="input-char" name="input-char" value={this.state.value} onChange={this.handleChange} required/>
-                        </div>
-    
-                        <div className="mb-3">
-                            <button type="submit" name="getInfoBtn" className="btn btn-primary search-btn">Submit</button>
-                        </div>
-                    </form>
-
-                    {this.state.Result ? (
-                        <figure>
-                        <img src={this.state.imgList[this.state.valueIndex]} alt-text="GOT Character"/>
-                        <figcaption className="figure-caption">
-                            {this.state.charName[this.state.valueIndex]}
-                        </figcaption>
-                        </figure>
-                        ) : ("")
-                    }
-                    {
-                        this.state.NoResult ? (
-                            <p>Please enter correct full name</p>
-                        ) : ("")
-                    }
-                </div>
+  return (
+    <div className="container w-75 mx-auto">
+      <h1>Search for the character</h1>
+      <div className="container-sm border rounded bg-light search-container">
+        <form onSubmit={handleSubmit} className="p-3">
+          <div className="mb-3">
+            <label htmlFor="input-char">Enter the character full name:</label>
+            <input
+              type="text"
+              id="input-char"
+              name="input-char"
+              value={value}
+              onChange={handleChange}
+              required
+            />
           </div>
-        );
-      }
-}
+
+          <div className="mb-3">
+            <button type="submit" name="getInfoBtn" className="btn btn-primary search-btn">
+              Submit
+            </button>
+          </div>
+        </form>
+
+        {result ? (
+          <figure>
+            <img src={imgList[valueIndex]} alt-text="GOT Character" />
+            <figcaption className="figure-caption">
+              {charName[valueIndex]}
+            </figcaption>
+          </figure>
+        ) : (
+          ""
+        )}
+        {noResult && <p>Please enter correct full name</p>}
+      </div>
+    </div>
+  );
+};
 
 export default Search;
